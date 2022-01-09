0b717313c39675f027221012612d0d7289061a75
Sam Smith <samsmithnz@gmail.com>
2022-01-09 14:35:33 -0500

    Renamed project to Core

---
+++ src/RepoGovernance.Service/Controllers/GitHubController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepoGovernance.Core.Models;
+
+namespace RepoGovernance.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GitHubController : ControllerBase
+    {
+        //public async Task<List<Repo>> GetRepos(string owner)
+        //{
+        //    RepoGovernance.Core.APIAccess.GitHubAPIAccess.GetRepo();
+        //}
+    }
+}
