28c68f7d391f313e0e83f2d65370ebd5fc6aed00
Sam Smith <samsmithnz@gmail.com>
2022-03-12 06:59:35 -0500

    Added recommendations to summary item

--- src/RepoGovernance.Core/Models/SummaryItem.cs
+++ src/RepoGovernance.Core/Models/SummaryItem.cs
@@ -8,17 +8,27 @@ namespace RepoGovernance.Core.Models
         {
             Repo = repo;
             Actions = new();
+            ActionRecommendations = new();
             Dependabot = new();
+            DependabotRecommendations = new();
             BranchPolicies = new();
+            BranchPoliciesRecommendations = new();
             GitVersion = new();
+            GitVersionRecommendations = new();
             Frameworks = new();
+            FrameworksRecommendations = new();
         }
 
         public string Repo { get; internal set; }
         public List<string> Actions { get; set; }
+        public List<string> ActionRecommendations { get; set; }
         public List<string> Dependabot { get; set; }
+        public List<string> DependabotRecommendations { get; set; }
         public BranchProtectionPolicy? BranchPolicies { get; set; }
+        public List<string> BranchPoliciesRecommendations { get; set; }
         public List<string> GitVersion { get; set; }
+        public List<string> GitVersionRecommendations { get; set; }
         public List<string> Frameworks { get; set; }
+        public List<string> FrameworksRecommendations { get; set; }
     }
 }
