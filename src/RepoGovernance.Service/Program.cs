91b6077451da233826c4d54107c6c47a1812bc46
Sam Smith <samsmithnz@gmail.com>
2022-03-14 07:20:20 -0400

    Added application insights

--- src/RepoGovernance.Service/Program.cs
+++ src/RepoGovernance.Service/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
 
 var app = builder.Build();
 
