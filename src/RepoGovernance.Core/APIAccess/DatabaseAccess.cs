23052575a45cc2758931e4d5bb615f1c0670f136
Sam Smith <samsmithnz@gmail.com>
2022-03-15 14:14:30 -0400

    Added extra repo, let's see what happens

--- src/RepoGovernance.Core/APIAccess/DatabaseAccess.cs
+++ src/RepoGovernance.Core/APIAccess/DatabaseAccess.cs
@@ -12,7 +12,8 @@ public static class DatabaseAccess
             "CustomQueue",
             "DevOpsMetrics",
             "RepoAutomation",
-            "samsmithnz"
+            "samsmithnz",
+            "TBS"
         };
 
         return results;
