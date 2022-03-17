682f00054b6e32bd9d8103a4d75effbe7d54770c
Sam Smith <samsmithnz@gmail.com>
2022-03-17 15:48:48 -0400

    Fixed bug

--- src/RepoGovernance.Function/ProcessQueueItem.cs
+++ src/RepoGovernance.Function/ProcessQueueItem.cs
@@ -1,14 +1,14 @@
-using Microsoft.Azure.WebJobs;
-using Microsoft.Extensions.Logging;
+//using Microsoft.Azure.WebJobs;
+//using Microsoft.Extensions.Logging;
 
-namespace RepoGovernance.Function
-{
-    public class ProcessQueueItem
-    {
-        [FunctionName("ProcessQueueItem")]
-        public void Run([QueueTrigger("summaryqueue", Connection = "AzureWebJobsStorage")] string myQueueItem, ILogger log)
-        {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
-        }
-    }
-}
+//namespace RepoGovernance.Function
+//{
+//    public class ProcessQueueItem
+//    {
+//        [FunctionName("ProcessQueueItem")]
+//        public void Run([QueueTrigger("summaryqueue", Connection = "AzureWebJobsStorage")] string myQueueItem, ILogger log)
+//        {
+//            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+//        }
+//    }
+//}
