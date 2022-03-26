03a26044fd41a5b0b772e9345e78f77153458000
Sam Smith <samsmithnz@gmail.com>
2022-03-25 22:24:56 -0400

    Code Gardening

--- src/RepoGovernance.Core/Models/Framework.cs
+++ src/RepoGovernance.Core/Models/Framework.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RepoGovernance.Core.Models
+﻿namespace RepoGovernance.Core.Models
 {
     public class Framework
     {
-        public string Name { get; set; }
-        public string Color { get; set; }
+        public string? Name { get; set; }
+        public string? Color { get; set; }
     }
 }
