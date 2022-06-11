b4642f0e672a91d6a447cf6bd6b5b90d4da76132
Sam Smith <samsmithnz@gmail.com>
2022-06-11 07:14:11 -0400

    Added total count

--- src/RepoGovernance.Core/Models/SummaryItem.cs
+++ src/RepoGovernance.Core/Models/SummaryItem.cs
@@ -46,15 +46,23 @@ namespace RepoGovernance.Core.Models
         public string? LastCommitSha { get; set; }
         public DateTime LastUpdated { get; set; }
 
+        public string OwnerRepo
+        {
+            get
+            {
+                return Owner + "_" + Repo;
+            }
+        }
+
         public int TotalRecommendationCount
         {
             get
             {
-                return RepoSettingsRecommendations.Count + 
+                return RepoSettingsRecommendations.Count +
                     ActionRecommendations.Count +
                     DependabotRecommendations.Count +
-                    BranchPoliciesRecommendations.Count + 
-                    GitVersionRecommendations.Count + 
+                    BranchPoliciesRecommendations.Count +
+                    GitVersionRecommendations.Count +
                     DotNetFrameworksRecommendations.Count;
             }
         }
