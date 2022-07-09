6c133c8520f10a88868d8d57a96127c010cff105
Sam Smith <samsmithnz@gmail.com>
2022-07-09 06:48:32 -0400

    Updated DevOps metrics processing to include defaults

--- src/RepoGovernance.Core/Models/DORASummaryItem.cs
+++ src/RepoGovernance.Core/Models/DORASummaryItem.cs
@@ -3,6 +3,21 @@
     //This class will mirror the result from DevOpsMetrics
     public class DORASummaryItem
     {
+        public DORASummaryItem(string owner, string repo)
+        {
+            Owner = owner;
+            Repo = repo;
+            //Set the properties to defaults (none)
+            DeploymentFrequencyBadgeURL = "https://img.shields.io/badge/Deployment%20frequency-None-lightgrey";
+            //DeploymentFrequencyBadgeWithMetricURL = deploymentFrequencyBadgeWithMetricURL;
+            LeadTimeForChangesBadgeURL = "https://img.shields.io/badge/Lead%20time%20for%20changes-None-lightgrey";
+            //LeadTimeForChangesWithMetricURL = leadTimeForChangesWithMetricURL;
+            MeanTimeToRestoreBadgeURL = "https://img.shields.io/badge/Time%20to%20restore%20service-None-lightgrey";
+            //MeanTimeToRestoreBadgeWithMetricURL = meanTimeToRestoreBadgeWithMetricURL;
+            ChangeFailureRateBadgeURL = "https://img.shields.io/badge/Change%20failure%20rate-None-lightgrey";
+            //ChangeFailureRateBadgeWithMetricURL = changeFailureRateBadgeWithMetricURL;
+        }
+
         public string Owner
         {
             get; set;
