5154455504b130e68ea4035bb2d4d2d0afbc354c
Sam Smith <samsmithnz@gmail.com>
2022-08-07 10:43:12 -0400

    Fixed test

--- src/RepoGovernance.Tests/SummaryItemsControllerTests.cs
+++ src/RepoGovernance.Tests/SummaryItemsControllerTests.cs
@@ -80,9 +80,9 @@ public class SummaryItemsControllerTests : BaseAPIAccessTests
         Assert.AreEqual(1, item1.GitVersion.Count);
         Assert.AreEqual(0, item1.GitVersionRecommendations.Count);
         Assert.AreEqual(2, item1.DotNetFrameworks.Count);
-        Assert.AreEqual("netstandard2.0", item1.DotNetFrameworks[0].Name);
+        Assert.AreEqual("net6.0", item1.DotNetFrameworks[0].Name);
         Assert.AreEqual("bg-primary", item1.DotNetFrameworks[0].Color);
-        Assert.AreEqual("net6.0", item1.DotNetFrameworks[1].Name);
+        Assert.AreEqual("netstandard2.0", item1.DotNetFrameworks[1].Name);
         Assert.AreEqual("bg-primary", item1.DotNetFrameworks[1].Color);
         Assert.AreEqual(0, item1.DotNetFrameworksRecommendations.Count);
         Assert.IsTrue(item1.PullRequests.Count >= 0);
