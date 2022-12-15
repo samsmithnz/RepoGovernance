e0628f3409b0a88f1c9f0404b4062d0ac84cda55
Sam Smith <samsmithnz@gmail.com>
2022-12-15 11:18:02 -0500

    fixed bug

--- src/RepoGovernance.Core/Models/DORASummaryItem.cs
+++ src/RepoGovernance.Core/Models/DORASummaryItem.cs
@@ -11,7 +11,7 @@
             DeploymentFrequencyBadgeURL = "https://img.shields.io/badge/Deployment%20frequency-None-lightgrey";
             DeploymentFrequencyBadgeWithMetricURL = "https://img.shields.io/badge/Deployment%20frequency%20%280.00%20times%20per%20year%29-None-lightgrey";
             LeadTimeForChangesBadgeURL = "https://img.shields.io/badge/Lead%20time%20for%20changes-None-lightgrey";
-            LeadTimeForChangesWithMetricURL = "https://img.shields.io/badge/Lead%20time%20for%20changes%20%280.0%20hours%29-None-lightgrey";
+            LeadTimeForChangesBadgeWithMetricURL = "https://img.shields.io/badge/Lead%20time%20for%20changes%20%280.0%20hours%29-None-lightgrey";
             MeanTimeToRestoreBadgeURL = "https://img.shields.io/badge/Time%20to%20restore%20service-None-lightgrey";
             MeanTimeToRestoreBadgeWithMetricURL = "https://img.shields.io/badge/Time%20to%20restore%20service%20%280.00%20hours%29-None-lightgrey";
             ChangeFailureRateBadgeURL = "https://img.shields.io/badge/Change%20failure%20rate-None-lightgrey";
