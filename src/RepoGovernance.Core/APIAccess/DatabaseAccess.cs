b99edd1558b273ef0ca7e6f90cd170f4eb9965cc
Sam Smith <samsmithnz@gmail.com>
2022-12-22 14:13:04 -0500

    Added SamsDotNetSonarCloudAction

--- src/RepoGovernance.Core/APIAccess/DatabaseAccess.cs
+++ src/RepoGovernance.Core/APIAccess/DatabaseAccess.cs
@@ -29,6 +29,7 @@ public static class DatabaseAccess
             new UserOwnerRepo(user, user, "SamsFeatureFlags"),
             new UserOwnerRepo(user, user, "samsmithnz"),
             new UserOwnerRepo(user, user, "SatisfactoryTree"),
+            new UserOwnerRepo(user, user, "SamsDotNetSonarCloudAction"),            
             new UserOwnerRepo(user, user, "TBS"),
             new UserOwnerRepo(user, "SamSmithNZ-dotcom", "SamSmithNZ.com"),
             new UserOwnerRepo(user, "SamSmithNZ-dotcom", "MandMCounter")
