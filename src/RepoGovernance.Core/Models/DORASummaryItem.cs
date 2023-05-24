7eb250150d66df79fc8b3a37edc2a02faa9b59cd
Sam Smith <samsmithnz@gmail.com>
2023-05-24 16:14:41 -0400

    Updated defaults for badges

--- src/RepoGovernance.Core/Models/DORASummaryItem.cs
+++ src/RepoGovernance.Core/Models/DORASummaryItem.cs
@@ -9,13 +9,13 @@
             Repo = repo;
             //Set the properties to defaults (none)
             DeploymentFrequencyBadgeURL = "https://img.shields.io/badge/Deployment%20frequency-None-lightgrey";
-            DeploymentFrequencyBadgeWithMetricURL = "https://img.shields.io/badge/Deployment%20frequency%20%280.00%20times%20per%20year%29-None-lightgrey";
+            DeploymentFrequencyBadgeWithMetricURL = "https://img.shields.io/badge/Deployment%20frequency%20-None-lightgrey";
             LeadTimeForChangesBadgeURL = "https://img.shields.io/badge/Lead%20time%20for%20changes-None-lightgrey";
-            LeadTimeForChangesBadgeWithMetricURL = "https://img.shields.io/badge/Lead%20time%20for%20changes%20%280.0%20hours%29-None-lightgrey";
+            LeadTimeForChangesBadgeWithMetricURL = "https://img.shields.io/badge/Lead%20time%20for%20changes%20-None-lightgrey";
             MeanTimeToRestoreBadgeURL = "https://img.shields.io/badge/Time%20to%20restore%20service-None-lightgrey";
-            MeanTimeToRestoreBadgeWithMetricURL = "https://img.shields.io/badge/Time%20to%20restore%20service%20%280.00%20hours%29-None-lightgrey";
+            MeanTimeToRestoreBadgeWithMetricURL = "https://img.shields.io/badge/Time%20to%20restore%20service%20-None-lightgrey";
             ChangeFailureRateBadgeURL = "https://img.shields.io/badge/Change%20failure%20rate-None-lightgrey";
-            ChangeFailureRateBadgeWithMetricURL = "https://img.shields.io/badge/Change%20failure%20rate%20%28100.00%25%29-None-lightgrey";
+            ChangeFailureRateBadgeWithMetricURL = "https://img.shields.io/badge/Change%20failure%20rate%20-None-lightgrey";
         }
 
         public string Owner
