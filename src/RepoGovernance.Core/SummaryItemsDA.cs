3b3aeb07a15ecd18c8d56eb283b8a3d9033bbaaa
Sam Smith <samsmithnz@github.com>
2023-07-29 13:16:22 -0400

    Adding count to .NET frameworks (#501)
    * Added count to code
    
    * Added counter to dotnet languages

--- src/RepoGovernance.Core/SummaryItemsDA.cs
+++ src/RepoGovernance.Core/SummaryItemsDA.cs
@@ -1,7 +1,6 @@
 ﻿using DotNetCensus.Core.Models;
 using GitHubActionsDotNet.Models.Dependabot;
 using GitHubActionsDotNet.Serialization;
-using Newtonsoft.Json;
 using RepoAutomation.Core.APIAccess;
 using RepoAutomation.Core.Helpers;
 using RepoAutomation.Core.Models;
@@ -220,7 +219,8 @@ namespace RepoGovernance.Core
                     Framework framework = new()
                     {
                         Name = frameworkSummary.Framework,
-                        Color = DotNetRepoScanner.GetColorFromStatus(frameworkSummary.Status)
+                        Color = DotNetRepoScanner.GetColorFromStatus(frameworkSummary.Status),
+                        Count = frameworkSummary.Count
                     };
                     if (frameworkSummary.Framework != null &&
                         summaryItem?.DotNetFrameworks.Where(p => p.Name == frameworkSummary.Framework).FirstOrDefault() == null)
