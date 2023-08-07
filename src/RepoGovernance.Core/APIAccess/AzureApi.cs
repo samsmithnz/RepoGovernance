3fd19d7b240cc63ddb3690694530add53c067ddb
Sam Smith <samsmithnz@gmail.com>
2023-08-07 19:07:15 -0400

    Added application api call

---
+++ src/RepoGovernance.Core/APIAccess/AzureApi.cs
@@ -0,0 +1,15 @@
+﻿using RepoGovernance.Core.Models.Azure;
+
+namespace RepoGovernance.Core.APIAccess
+{
+
+    public class AzureApi : BaseApi
+    {
+        public async Task<List<Application>> GetApplications()
+        {
+            string url = "https://graph.microsoft.com/v1.0/applications";
+            return await BaseApi.GetResponse<Application>(Client, url);
+
+        }
+    }
+}
---
+++ src/RepoGovernance.Core/Models/Azure/Application.cs
@@ -0,0 +1,23 @@
+﻿namespace RepoGovernance.Core.Models.Azure
+{
+    public class Application
+    {
+        //https://learn.microsoft.com/en-us/graph/api/application-list?view=graph-rest-1.0&tabs=http#http-request
+        //response
+        //        {
+        //  "@odata.context": "https://graph.microsoft.com/v1.0/$metadata#applications",
+        //  "value": [
+        //    {
+        //      "appId": "00000000-0000-0000-0000-000000000000",
+        //      "identifierUris": [ "http://contoso/" ],
+        //      "displayName": "My app",
+        //      "publisherDomain": "contoso.com",
+        //      "signInAudience": "AzureADMyOrg"
+        //    }
+        //  ]
+        //}
+
+        public string? appId { get; set; }
+        public string? displayName { get; set; }
+    }
+}
