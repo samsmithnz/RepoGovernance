60edd85767bc8b4a71f552873518169bc78ec944
Sam Smith <samsmithnz@gmail.com>
2023-08-07 14:44:38 -0400

    Added new classes

---
+++ src/RepoGovernance.Core/Models/AzureAppRegistration.cs
@@ -0,0 +1,15 @@
+﻿namespace RepoGovernance.Core.Models
+{
+    public class AzureAppRegistration
+    {
+        public string Name { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string ExpirationDateString
+        {
+            get
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ src/RepoGovernance.Core/Models/AzureDeployment.cs
@@ -0,0 +1,9 @@
+﻿namespace RepoGovernance.Core.Models
+{
+    public class AzureDeployment
+    {
+        public List<AzureAppRegistration> AppRegistrations { get; set; }
+        public string? DeployedURL { get; set; }
+
+    }
+}
