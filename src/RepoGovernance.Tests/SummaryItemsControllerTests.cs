3281e1bdafd1d402e9bbf0a8eec379a6bcde5556
Sam Smith <samsmithnz@gmail.com>
2023-08-30 21:04:54 -0400

    fix to test

--- src/RepoGovernance.Tests/SummaryItemsControllerTests.cs
+++ src/RepoGovernance.Tests/SummaryItemsControllerTests.cs
@@ -231,7 +231,7 @@ public class SummaryItemsControllerTests : BaseAPIAccessTests
             Assert.AreEqual("dependabot.yml", item1.DependabotFile?.name);
             Assert.AreEqual("2", item1.DependabotRoot?.version);
             Assert.IsTrue(item1.DependabotFile?.content?.Length > 0);
-            Assert.AreEqual(1, item1.DependabotRecommendations.Count);
+            Assert.AreEqual(0, item1.DependabotRecommendations.Count);
             Assert.AreEqual("Consider adding an open_pull_requests_limit to ensure Dependabot doesn't open too many PR's in the / project, github-actions ecosystem", item1.DependabotRecommendations[0]);
             Assert.IsNotNull(item1.BranchPolicies);
             Assert.AreEqual(0, item1.BranchPoliciesRecommendations.Count);
