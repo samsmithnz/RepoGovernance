b16b5b7d1469f25015ec8b60bff7991508493b72
Sam Smith <samsmithnz@gmail.com>
2025-04-25 07:21:36 -0400

    Fix to covered percent. Start of adding a details page

--- src/RepoGovernance.Core/Models/CoverallsCodeCoverage.cs
+++ src/RepoGovernance.Core/Models/CoverallsCodeCoverage.cs
@@ -5,21 +5,21 @@
         public string? branch { get; set; }
         public string? repo_name { get; set; }
         public string? badge_url { get; set; }
-        public double coverage_change { get; set; }
-        public double covered_percent { get; set; }
+        public double coverage_change { get; set; } = 0.0;
+        public double covered_percent { get; set; } = 0.0;
         public string coverage_change_string
         {
             get
             {
                 if (coverage_change < 0)
                 {
-                    return "-" + coverage_change.ToString("0");
+                    return "-" + coverage_change?.ToString("0");
                 }
                 else
                 {
-                    return "+" + coverage_change.ToString("0");
+                    return "+" + coverage_change?.ToString("0");
                 }
-            }
             }
         }
     }
+}
