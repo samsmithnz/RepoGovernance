11d600268fa163b1cf41d1d4463f108972729bd6
Sam Smith <samsmithnz@gmail.com>
2025-05-14 22:31:22 -0400

    Added new test for projects

---
+++ src/RepoGovernance.Tests/ProjectTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoGovernance.Core.Models;
+
+namespace RepoGovernance.Tests.Models
+{
+    [TestClass]
+    public class ProjectTests
+    {
+        [TestMethod]
+        public void Project_PropertySettersAndGetters_WorkCorrectly()
+        {
+            // Arrange
+            var project = new Project();
+
+            // Act
+            project.FileName = "MyProject.csproj";
+            project.Path = "/src/MyProject/MyProject.csproj";
+            project.Content = "<Project Sdk=\"Microsoft.NET.Sdk\"></Project>";
+            project.Framework = ".NET 8.0";
+            project.Color = "bg-primary";
+
+            // Assert
+            Assert.AreEqual("MyProject.csproj", project.FileName);
+            Assert.AreEqual("/src/MyProject/MyProject.csproj", project.Path);
+            Assert.AreEqual("<Project Sdk=\"Microsoft.NET.Sdk\"></Project>", project.Content);
+            Assert.AreEqual(".NET 8.0", project.Framework);
+            Assert.AreEqual("bg-primary", project.Color);
+        }
+    }
+}
